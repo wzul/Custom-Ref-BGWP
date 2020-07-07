4b9747dea1c9916afecc270ab7663901336076a8
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2020-07-08 02:22:17 +0800

    Update logic.php

--- includes/logic.php
+++ includes/logic.php
@@ -12,13 +12,17 @@ class CRBGWP_Logic
       'reference_1' => 'billplz_reference_1', 
       'reference_2' => 'billplz_reference_2',
       'reference_1_label' => 'billplz_reference_1_label',
-      'reference_2_label' => 'billplz_reference_2_label',
+      'reference_2_label' => 'billplz_reference_2_label'
+    ];
+    
+    const MANDATORY_KEY = [
       'mobile' => 'billplz_mobile'
     ];
 
     private function __construct()
     {
         add_filter('give_billplz_bill_optional_param', array($this, 'filter_reference'), 10, 2);
+        add_filter('give_billplz_bill_mandatory_param', array($this, 'filter_mandatory'), 10, 2);
     }
 
     public static function get_instance()
@@ -33,6 +37,16 @@ class CRBGWP_Logic
     public function filter_reference($optional, $post_data)
     {
       foreach (self::KEY as $key => $value){
+        if (isset($post_data[$value]) && !empty($post_data[$value])){
+          $optional[$key] = $this->identify_post_data_type($post_data[$value]);
+        }
+      }
+      return $optional;
+    }
+    
+    public function filter_mandatory($optional, $post_data)
+    {
+      foreach (self::MANDATORY_KEY as $key => $value){
         if (isset($post_data[$value]) && !empty($post_data[$value])){
           $optional[$key] = $this->identify_post_data_type($post_data[$value]);
         }
